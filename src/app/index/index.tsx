2be5c316d068e7eed0e03dd47cfeaf90d50ab250
Jefferson Araujo <jef9296@gmail.com>
2026-07-01 21:07:09 -0300

    Adicionado função de salvar links

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
+  Alert,
   FlatList,
   Image,
   Modal,
@@ -12,15 +13,31 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
 
 import { styles } from "@/app/index/styles";
+import { Categories } from "@/components/categories";
+import { linkStorage, LinkStorage } from "@/storage/link-storage";
 import { colors } from "@/styles/colors";
 
-import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { Option } from "@/components/option";
 import { categories } from "@/utils/categories";
 
 export default function Index() {
+  const [links, setLinks] = useState<LinkStorage[]>([]);
   const [category, setCategory] = useState(categories[0].name);
+
+  async function getLinks() {
+    try {
+      const response = await linkStorage.get();
+      setLinks(response);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível listar os links");
+    }
+  }
+
+  useEffect(() => {
+    getLinks();
+  }, [category]);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -34,12 +51,12 @@ export default function Index() {
       <Categories onChange={setCategory} selected={category} />
 
       <FlatList
-        data={["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]}
-        keyExtractor={(item) => item}
-        renderItem={() => (
+        data={links}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
           <Link
-            name="Rocketseat"
-            url="https://www.rocketseat.com.br/"
+            name={item.name}
+            url={item.url}
             onDetails={() => console.log("Clicou!")}
           />
         )}
